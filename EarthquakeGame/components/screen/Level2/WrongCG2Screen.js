eef269d2a20aa1f988d8ad84334740e8fff8fd32
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-05 01:21:06 +0800

    加上第二關失敗畫面

--- EarthquakeGame/components/screen/Level2/WrongCG2Screen.js
+++ EarthquakeGame/components/screen/Level2/WrongCG2Screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Pressable, View, Platform, } from 'react-native';
+import { StyleSheet, Pressable, View, Platform, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Text, VStack, HStack } from '@gluestack-ui/themed';
 import { Button, ButtonText, ButtonIcon, ButtonSpinner, ButtonGroup } from '@gluestack-ui/themed';
@@ -14,14 +14,25 @@ const WrongCG2Screen = () => {
 
     return (
         <View>
-            <Text>WrongCG2Screen</Text>
-            <Text>{Answer2}</Text>
-            <Pressable onPress={() => navigation.navigate('Talk3_1Screen')}>
-                <Text style={{ height: 50, width: 200, backgroundColor: '#000', color: '#fff' }}>前往第三關</Text>
-            </Pressable>
+            <ImageBackground
+                style={{ width: '100%', height: '100%' }}
+                source={require('../../images/20w.png')}
+            >
+                <Pressable onPress={() => navigation.navigate('Talk3_1Screen')}>
+                    <Text style={styles.detection}></Text>
+                </Pressable>
+            </ImageBackground>
         </View>
 
     );
 }
-
+const styles = StyleSheet.create({
+    detection: {
+        height: '60%',
+        width: '100%',
+        marginTop: '80%',
+        backgroundColor: 'transparent',
+        color: '#fff'
+    }
+})
 export default WrongCG2Screen;
