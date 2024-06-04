197c4e61fd6c212a8e9bc7fe2a1edfc3b5f337c5
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-05 01:33:53 +0800

    第四關Option完成

--- EarthquakeGame/components/screen/Level4/Option4Screen.js
+++ EarthquakeGame/components/screen/Level4/Option4Screen.js
@@ -1,21 +1,41 @@
 import React from "react";
-import { StyleSheet, Pressable, View, Platform, } from 'react-native';
-import { useNavigation} from '@react-navigation/native';
+import { StyleSheet, Pressable, View, Platform, Image, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Text, VStack, HStack } from '@gluestack-ui/themed';
 import { Button, ButtonText, ButtonIcon, ButtonSpinner, ButtonGroup } from '@gluestack-ui/themed';
+import { useDispatch, useSelector } from "react-redux";
+import { selectAnswer4 } from "../../redux/Slice";
+import { correct4Counter, wrong4Counter } from "../../redux/Slice";
 
 const Option4Screen = () => {
     const navigation = useNavigation();
+    const Answer4 = useSelector(selectAnswer4);
+    const dispatch = useDispatch();
 
-    return(
+    return (
         <View>
-            <Text>Option4Screen</Text>
-            <Pressable onPress={() => navigation.navigate('RightCG4Screen')}>
-                <Text style={{height:50, width:200, backgroundColor:'#000', color:'#fff'}}>正確</Text>
-            </Pressable>
-            <Pressable onPress={() => navigation.navigate('WrongCG4Screen')}>
-                <Text style={{height:50, width:200, backgroundColor:'#000', color:'#fff'}}>錯誤</Text>
-            </Pressable>
+            <ImageBackground
+                style={{ width: '100%', height: '100%' }}
+                source={require('../../images/40o.png')}
+            >
+                <View style={{ flexDirection: 'column', margin: 'auto' ,marginBottom:160}}>
+                    <Pressable onPress={() => { navigation.navigate('RightCG4Screen'); dispatch(wrong4Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項一</Text>
+                    </Pressable>
+                    <Pressable onPress={() => { navigation.navigate('WrongCG4Screen'); dispatch(wrong4Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項二</Text>
+                    </Pressable>
+
+                    <Pressable onPress={() => { navigation.navigate('WrongCG4Screen'); dispatch(correct4Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項二</Text>
+                    </Pressable>
+                    <Pressable onPress={() => { navigation.navigate('WrongCG4Screen'); dispatch(wrong4Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent' }}>選項二</Text>
+                    </Pressable>
+
+                </View>
+
+            </ImageBackground>
         </View>
 
     );
