de5afcc88da0a1c04d33c3efb5c7701ea9fdada4
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-05 02:06:07 +0800

    放照片+第一關Option完成

--- EarthquakeGame/components/screen/Level1/Option1Screen.js
+++ EarthquakeGame/components/screen/Level1/Option1Screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Pressable, View, Platform, } from 'react-native';
+import { StyleSheet, Pressable, View, Platform, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Text, VStack, HStack } from '@gluestack-ui/themed';
 import { Button, ButtonText, ButtonIcon, ButtonSpinner, ButtonGroup } from '@gluestack-ui/themed';
@@ -9,27 +9,28 @@ import { correct1Counter, wrong1Counter } from "../../redux/Slice";
 
 const Option1Screen = () => {
     const navigation = useNavigation();
-    // Get states from store
     const Answer1 = useSelector(selectAnswer1);
-    // Define a dispatch to send actions
     const dispatch = useDispatch();
 
     return (
         <View>
-            <Text>Option1Screen</Text>
-            <Pressable onPress={() => { navigation.navigate('RightCG1Screen'); dispatch(correct1Counter()); }}>
-                <Text style={{ height: 50, width: 200, backgroundColor: '#000', color: '#fff' }}>選項一</Text>
-            </Pressable>
-            <Pressable onPress={() => {navigation.navigate('WrongCG1Screen'); dispatch(wrong1Counter());}}>
-                <Text style={{ height: 50, width: 200, backgroundColor: '#000', color: '#fff' }}>選項二</Text>
-            </Pressable>
-            <Pressable onPress={() => {navigation.navigate('WrongCG1Screen'); dispatch(wrong1Counter());}}>
-                <Text style={{ height: 50, width: 200, backgroundColor: '#000', color: '#fff' }}>選項三</Text>
-            </Pressable>
-            <Pressable onPress={() => {navigation.navigate('WrongCG1Screen'); dispatch(wrong1Counter());}}>
-                <Text style={{ height: 50, width: 200, backgroundColor: '#000', color: '#fff' }}>選項四</Text>
-            </Pressable>
-            <Text>{Answer1}</Text>
+            <ImageBackground
+                style={{ width: '100%', height: '100%' }}
+                source={require('../../images/10o.png')}
+            >
+                <View style={{ flexDirection: 'column', margin: 'auto' ,marginBottom:280}}>
+                    <Pressable onPress={() => { navigation.navigate('RightCG1Screen'); dispatch(correct1Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項一</Text>
+                    </Pressable>
+                    <Pressable onPress={() => { navigation.navigate('WrongCG1Screen'); dispatch(wrong1Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項二</Text>
+                    </Pressable>
+                    <Pressable onPress={() => { navigation.navigate('WrongCG1Screen'); dispatch(wrong1Counter()); }}>
+                        <Text style={{ height: 90, width: 280, backgroundColor: 'transparent', color: 'transparent',marginBottom:40 }}>選項二</Text>
+                    </Pressable>
+                </View>
+
+            </ImageBackground>
         </View>
 
     );
