c6004d7eb36df6c5724e297428311c5ac46749bf
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-04 17:39:48 +0800

    放照片成功

--- EarthquakeGame/components/screen/Level2/Talk2_1Screen.js
+++ EarthquakeGame/components/screen/Level2/Talk2_1Screen.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { StyleSheet, Pressable, View, Platform, } from 'react-native';
+import { StyleSheet, Pressable, View, Platform,Image, ImageBackground } from 'react-native';
 import { useNavigation} from '@react-navigation/native';
 import { Text, VStack, HStack } from '@gluestack-ui/themed';
 import { Button, ButtonText, ButtonIcon, ButtonSpinner, ButtonGroup } from '@gluestack-ui/themed';
+
 
 const Talk2_1Screen = () => {
     const navigation = useNavigation();
 
     return(
         <View>
-            <Text>Talk2_1Screen</Text>
+            <ImageBackground
+            style={{width: '100%', height: '100%'}}
+            source={require('../../images/201.png')}
+            >
+            <Text style={{color:'black',backgroundColor:'#B3B3B3'}}>Talk2_1Screen</Text>
             <Pressable onPress={() => navigation.navigate('Puzzle2Screen')}>
                 <Text style={{height:50, width:200, backgroundColor:'#000', color:'#fff'}}>開始解謎</Text>
             </Pressable>
             <Pressable onPress={() => navigation.navigate('Talk3_1Screen')}>
                 <Text style={{height:50, width:200, backgroundColor:'#999999', color:'#fff'}}>去第三關</Text>
             </Pressable>
+            </ImageBackground>
         </View>
 
     );
