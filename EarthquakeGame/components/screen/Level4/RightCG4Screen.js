90794c06a57b2e6f4fa6fdb8e8334964f671a3ad
xiaaaaaa <78157957+xiaaaaaa@users.noreply.github.com>
2024-06-05 03:35:55 +0800

    第四關完成

--- EarthquakeGame/components/screen/Level4/RightCG4Screen.js
+++ EarthquakeGame/components/screen/Level4/RightCG4Screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Pressable, View, Platform, } from 'react-native';
+import { StyleSheet, Pressable, View, Platform,Image, ImageBackground } from 'react-native';
 import { useNavigation} from '@react-navigation/native';
 import { Text, VStack, HStack } from '@gluestack-ui/themed';
 import { Button, ButtonText, ButtonIcon, ButtonSpinner, ButtonGroup } from '@gluestack-ui/themed';
@@ -9,14 +9,30 @@ const RightCG4Screen = () => {
 
     return(
         <View>
-            <Text>RightCG4Screen</Text>
-            <Pressable onPress={() => navigation.navigate('TalkE_1Screen')}>
-                <Text style={{height:50, width:200, backgroundColor:'#000', color:'#fff'}}>前往結尾</Text>
-            </Pressable>
-            
+            <ImageBackground
+                style={{width: '100%', height: '100%'}}
+                source={require('../../images/40r.png')}
+            >
+                <Pressable onPress={() => navigation.navigate('TalkE_1Screen')}>
+                    <Text style={styles.detection}></Text>
+                </Pressable>
+            </ImageBackground>
+
         </View>
 
     );
 }
+
+const styles = StyleSheet.create({
+    detection: { 
+        height:'60%', 
+        width:'100%', 
+        marginTop:'80%',
+        backgroundColor:'transparent', 
+        color:'#fff'
+    }
+})
+
+
 
 export default RightCG4Screen;
